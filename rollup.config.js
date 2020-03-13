6ac1fddc2a8ce17bfb7b5dbc62f7f40854cedec4
Rogério Luiz Aques de Amorim <rogeriol@gmail.com>
2020-03-13 18:40:16 -0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ import { terser } from "rollup-plugin-terser";
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: "index.js",
+  input: "./example/index.js",
   output: {
     sourcemap: true,
     format: "iife",
